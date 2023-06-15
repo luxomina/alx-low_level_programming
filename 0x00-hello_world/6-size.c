a11cd011a3543f6f0d8ec859d545a35aedd6bb9e
luxomina <luxomina@gmail.com>
2023-06-15 07:37:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,12 @@
  * main - program that prints the size of types
  * Return: (0) Success
  */
-int main(void) {
-    printf("size of a char: %zu byte(s)\n", sizeof(char));
-    printf("size of an int: %zu byte(s)\n", sizeof(int));
-    printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("size of a float: %zu byte(s)\n", sizeof(float));
-    return 0;
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
