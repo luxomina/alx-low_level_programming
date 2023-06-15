dddf2894e1519d0e80dd0d3e064887f1bf167eb1
luxomina <luxomina@gmail.com>
2023-06-15 06:26:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A program to use printf
+ * Return: 0 (Sucees)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
