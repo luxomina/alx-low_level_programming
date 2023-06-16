9a59c7e1113f76e19c9b7adb3d4fb8f2957db5a0
luxomina <luxomina@gmail.com>
2023-06-16 08:15:58 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <std.io>
+/**
+ * main - program to print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char = letters;
+
+	for (letters = 'a' ; letters <= 'z' ; letters++)
+	{
+		if (letters != q && letters != e)
+		{
+			putchat(letters);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
