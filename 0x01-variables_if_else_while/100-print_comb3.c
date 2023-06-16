247587baecbb3bc601b17c12076905f23cd2f3a8
luxomina <luxomina@gmail.com>
2023-06-16 10:02:13 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program to print all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0 ; x <= 8 ; x++)
+	{
+		for (y = x + 1 ; y <= 9 ; y++)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
