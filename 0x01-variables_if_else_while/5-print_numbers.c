abf1ec33e8781ec9f591cf76e0d5c220857a96f7
luxomina <luxomina@gmail.com>
2023-06-16 08:43:40 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program to prints all single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
