f01b57add7dc1869f15ae05eb263713062d807cb
luxomina <luxomina@gmail.com>
2023-06-20 12:01:15 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @x: input integer
+ * Return: Always 0 (Success)
+ */
+int _abs(int x)
+{
+if (i < 0)
+{
+return (x * -1);
+}
+_putchar('\n');
+return (n);
+}
