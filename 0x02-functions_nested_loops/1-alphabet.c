40b336cd18b4af28088e998cc67d7dae51226879
luxomina <luxomina@gmail.com>
2023-06-20 10:54:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Special Entry
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+-putchar(x);
+}
+-putchar('\n');
+}
