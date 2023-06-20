643644b8fcb1852b0a45aeaa60db8c366f816957
luxomina <luxomina@gmail.com>
2023-06-20 10:16:42 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "_putchar";
+int x;
+for (x = 0; x < 8; x++)
+{_putchar(str[x]);
+_putchar('\n');
+return (0);
+}
