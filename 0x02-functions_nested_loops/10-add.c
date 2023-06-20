b7d2e08aef4ec743c210c93e3316f660dd11a1e1
luxomina <luxomina@gmail.com>
2023-06-20 13:25:18 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *@x: first integer
+ *@y: second integer
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+int sum = x + y;
+return (sum);
+}
