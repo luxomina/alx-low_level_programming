6afa98789c32fb7be8fdf9c5b15e769ba45ce056
luxomina <luxomina@gmail.com>
2023-06-20 13:20:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,15 +16,11 @@ _putchar(',');
 _putchar(' ');
 prod = num * mult;
 if (prod <= 9)
-{
 _putchar(' ');
-}
 else
-{
 _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
-}
 }
 }
