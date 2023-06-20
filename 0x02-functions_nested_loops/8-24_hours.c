7a169f1bf6b8b2ec60b86a7d66e7c63112d9f94f
luxomina <luxomina@gmail.com>
2023-06-20 12:43:59 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+int min, hr;
+
+for (hr = 0; hr <= 23; hr++)
+{
+for (min = 0; min <= 59; min++)
+{
+_purchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
+}
