1f9eec81d7998777f299cf85f9fe814ac29f4511
luxomina <luxomina@gmail.com>
2023-06-22 07:48:46 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,18 @@ int main(void)
 int x;
 for (x = 1; x <= 100; x++)
 {
-if (x % 3 == 0)
-{
-printf("Fizz ");
-}
-else if (x % 5 == 0)
-{
-printf("Buzz ");
-}
-else if (x % 3 == 0 && x % 5 == 0)
-{
-printf("FizzBuzz ");
-}
+if (x % 3 == 0 && !(x % 5 == 0))
+printf("Fizz");
+if (x % 5 == 0 && !(x % 3 == 0))
+printf("Buzz");
+if (x % 3 == 0 && x % 5 == 0)
+printf("FizzBuzz");
 else
-{
-printf("%d ", x);
-}
-}
+printf("%d", x);
+if (x != 100)
+printf(" ");
+else
 printf("\n");
+}
 return (0);
 }
