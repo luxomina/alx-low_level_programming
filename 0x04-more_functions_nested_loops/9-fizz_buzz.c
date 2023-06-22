9488c278c3d04acdbb44ac6abd1c4d0736d4901d
luxomina <luxomina@gmail.com>
2023-06-22 07:39:12 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,21 @@ for (x = 1; x <= 100; x++)
 {
 if (x % 3 == 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else if (x % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else if (x % 3 == 0 && x % 5 == 0)
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else
 {
-printf("%d", x);
-}
-if (x < 100)
-{
-printf(" ");
+printf("%d ", x);
 }
 }
+printf("\n");
 return (0);
 }
