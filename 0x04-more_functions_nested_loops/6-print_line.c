457ced47aa35de7ffdb97b942d900c8ab1f72411
luxomina <luxomina@gmail.com>
2023-06-22 06:14:46 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: is the number of times the characte
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+int x;
+
+if (x <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar('_');
+}
+}
+}
