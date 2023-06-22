ea48afabf643c808c6753510cd660a1f3852ba1e
luxomina <luxomina@gmail.com>
2023-06-22 06:48:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
  * print_diagonal - Entry point
+ *@n: is the number of times the character \ should be printed
  * Return: Always 0 (Success)
  */
 void print_diagonal(int n)
 {
 int x, y;
 
-if (n <=0)
-{
+if (n <= 0)
 _putchar('\n');
-}
-else
-{
 for (x = 0; x < n; x++)
 {
 for (y = 0; y < x; y++)
@@ -20,7 +17,6 @@ for (y = 0; y < x; y++)
 _putchar(' ');
 }
 _putchar('\\');
-}
 _putchar('\n');
 }
 }
