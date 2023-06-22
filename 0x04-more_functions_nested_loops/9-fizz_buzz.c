16c2777e4c7820358494cc3ba3caab6eacdb758c
luxomina <luxomina@gmail.com>
2023-06-22 07:34:33 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else if (x % 3 == 0 && x % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", x);
+}
+if (x < 100)
+{
+printf(" ");
+}
+}
+return (0);
+}
