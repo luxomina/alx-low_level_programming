c3d1d991a1adba2be0136fd8ba3202b4d38c1378
luxomina <luxomina@gmail.com>
2023-06-22 08:16:36 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,12 +9,10 @@ void print_triangle(int size)
 int height, base;
 
 if (size <= 0)
-{
 _putchar('\n');
-}
 else
 {
-for (height = 1; height < size; height++)
+for (height = 1; height <= size; height++)
 {
 for (base = 1; base <= size; base++)
 {
