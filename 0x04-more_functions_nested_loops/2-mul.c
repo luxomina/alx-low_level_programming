569d418dfd89012042bf65a0cc3f9a3fc04db671
luxomina <luxomina@gmail.com>
2023-06-22 03:24:28 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - Entry point
+ *@a: multiplies two integers
+ *@b: multiplies two integers
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
