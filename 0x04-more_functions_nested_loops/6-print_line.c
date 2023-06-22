4c9e723121f3b837c0485be47bb8dcfe90008f90
luxomina <luxomina@gmail.com>
2023-06-22 06:24:10 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,11 @@
  */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (int x = 0; x < n; x++)
+int x;
+
+for (x = 0; x < n; x++)
 {
 _putchar('_');
 }
 _putchar('\n');
-}
 }
