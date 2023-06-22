045b02fdc9d8e29c449dced8205d4a9a449c75e0
luxomina <luxomina@gmail.com>
2023-06-22 02:41:20 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *@c: uppercase character
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= ''Z)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
