5d5b7bb506af51deecd0549935ef6ec48ce866e1
luxomina <luxomina@gmail.com>
2023-06-22 02:57:35 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ *@c: is a digit
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+if (C >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
