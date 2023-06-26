e99d22b230fd54d10701acc9ce9c9bf779bb0c3b
luxomina <luxomina@gmail.com>
2023-06-26 11:16:53 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int sum;
+char c;
+
+srand(time(NULL));
+while (sum <= 2645)
+{
+c = rand % 120;
+sum += c;
+putchar(c);
+}
+putchar(2772 - sum);
+return (0);
+}
