2021fa9154804bdbd294ed89eae02129c4570bf9
luxomina <luxomina@gmail.com>
2023-06-26 10:42:53 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - Entry point
+ *@n: prints n elements of an array of integers
+ *@a: prints n elements of an array of integers
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+int x;
+
+for (x = 0; x < n; x++)
+{
+if (x != n - 1)
+printf("%d, ", a[x]);
+else
+printf("%d, ", a[x]);
+}
+printf("\n");
+}
