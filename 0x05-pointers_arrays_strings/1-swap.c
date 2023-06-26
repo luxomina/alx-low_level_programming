4d6a318b6581a3c1b7bce5341585b986fa1e545b
luxomina <luxomina@gmail.com>
2023-06-26 09:04:10 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ *@a:  swaps the values of two integers
+ *@b:  swaps the values of two integers
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+int swap;
+
+*a = swap;
+*a = *b;
+*b = swap;
+}
