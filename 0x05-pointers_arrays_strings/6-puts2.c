cb35567c2eb8a849c4cbbda9462eb229f8963bae
luxomina <luxomina@gmail.com>
2023-06-26 10:11:09 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ *@str: prints every other character of a string
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+int x;
+
+for (x = 0; str[x] != '\0'; x++)
+{
+if (x % 2 == 0)
+{
+_putchar(str[x]);
+}
+_putchar('\n');
+}
+}
